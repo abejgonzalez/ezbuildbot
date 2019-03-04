49ec8bbde0b5e62a1a0c393728d467278bc2b3c9
Edward Wang <edward.c.wang@compdigitec.com>
2019-03-03 18:02:01 -0800

    Add initial worker build script

---
+++ build-worker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Rebuild the buildbot worker image.
+set -ex
+set -euo pipefail
+
+source defaults.sh
+
+cp $(readlink -f $BUILDBOT_CONFIG) $BUILD_TEMPDIR/master.cfg
+cat > $BUILD_TEMPDIR/Dockerfile.worker <<EOF
+# Worker Dockerfile
+
+FROM ${PROJ_PREFIX}-worker-base
+
+USER root
+
+# Make git non-interactive
+RUN bash -c 'echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> /etc/ssh/ssh_config'
+ENV GIT_TERMINAL_PROMPT 0
+
+# TODO(edwardw): add hook to make this parametrizable
+
+# Run image (copied from parent Dockerfile)
+USER buildbot
+WORKDIR /buildbot
+
+CMD ["/usr/bin/dumb-init", "twistd", "--pidfile=", "-ny", "buildbot.tac"]
+EOF
+
+cd $BUILD_TEMPDIR
+docker build -t "${PROJ_PREFIX}-worker" -f Dockerfile.worker .
